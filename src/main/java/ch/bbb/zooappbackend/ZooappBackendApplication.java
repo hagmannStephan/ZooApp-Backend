808324080e6a9e197a03a520b5e418e5986ba3bb
Stephan <stephanhagmann@outlook.com>
2023-12-15 08:42:44 +0100

    Inital Commit

---
+++ src/main/java/ch/bbb/zooappbackend/ZooappBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbb.zooappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbb/zooappbackend/ZooappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbb.zooappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
